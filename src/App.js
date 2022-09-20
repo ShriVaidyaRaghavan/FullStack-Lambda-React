6ee0bf09232fca1ae7b4774461dcfeb7d2dd924d
ShriVaidyaRaghavan <vaidyaraghavan@outlook.com>
2022-09-20 05:39:04 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { useState } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-function App() {
+// class App extends Component {
+
+//   constructor(props) {
+//     super(props);
+    
+//     this.state = {
+//       id:'',
+//       Name:''
+//     };
+//   }
+
+//   componentDidMount() {
+//     axios
+//       .get('https://9uysf0rwyg.execute-api.us-east-1.amazonaws.com/dev/customers/')
+//       .then((response) => {
+//         this.setState({
+//             id:response.data.id,
+//             Name:response.data.Name
+//         })
+//         console.log(response.data.Items);
+//     })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//     }
+//   render() {
+//     const {id} = this.state;
+//     const {Name} = this.state;
+//     return (
+//       <div className="App">
+//       <p>Id:{id}</p>
+//       <p>Name:{Name}</p>
+//       </div>
+//     );
+//   }
+// }
+
+function App(){
+  const [input, setInput] = useState("")
+  const [customers, setCustomers] = useState([])
+
+  const myAPI = 'https://9uysf0rwyg.execute-api.us-east-1.amazonaws.com/dev/customers'
+  
+  // const getCustomer = (e) => 
+  function getCustomer( ) {
+    let customerId = input;
+    axios
+      .get(myAPI + "/" + customerId)
+      .then((response) => {
+        console.log(myAPI + "/" + customerId);
+        console.log(response.data.Items);
+        let newCustomers = [...customers]
+         newCustomers.push(response.data)
+         setCustomers(newCustomers)
+    })
+      .catch((error) => {
+        console.log(error);
+      });
+    }
+
+    function handleChange(event){
+      setInput(event.target.value);
+    }
+  
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Customer Id</h1>
+     <div>
+          <input placeholder="customer id" type="text" value={input} onChange={handleChange}/>      
+      </div>
+      <br/>
+     <button onClick={getCustomer}>Get Customers</button> 
+     {<p>{JSON.stringify(customers)}</p>} 
     </div>
   );
 }
-
 export default App;
